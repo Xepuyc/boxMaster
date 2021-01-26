9c6913174a5ea2c35add7ed7e94702e9968e095c
Xepuyc <73747014+Xepuyc@users.noreply.github.com>
2021-01-26 13:23:37 +0700

    1
    1

---
+++ BoxMaster/CalculationBOX.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BoxMaster
+{
+    class Count
+    {
+        public static double length (int s, int v, int F )
+            
+        {
+            double s1 = s * 0.0001;
+            double v1 = v * 0.001;
+            double D1 = Math.Sqrt(s1 / Math.PI) * 2;
+            double a = 343 / (2 * Math.PI);
+            double c=Math.Pow(F/a,2);
+            double l = (s1/c/v1*100)-(0.85*D1*100);       
+            
+            return l;   
+
+        }
+        public static double area (int l, int v, int F)
+        {
+
+            double a = 343 / (2 * Math.PI);
+            double c = Math.Pow(F / a, 2);
+            double s = l*v*c/10;
+            return s;
+
+        }
+        public static double Volume (int l, int s, int F)
+        {
+            double s1 = s * 0.0001;
+            double a = 343 / (2 * Math.PI);
+            double c = Math.Pow(F / a, 2);
+            double D1 = Math.Sqrt(s1 / Math.PI) * 2;
+            double V = s1 / (l/100 - 0.85 * D1) / c;
+            return V;
+
+        }
+        public static double Freq (int l, int s, int V)
+        {
+            double s1 = s * 0.0001;
+            double l1 = l *0.01;
+            double v1 = V * 0.001;
+            double D1 = Math.Sqrt(s1 / Math.PI) * 2;
+            double z = (343 / (2 * Math.PI));
+            
+            double F= z*   Math.Sqrt(s1 / ((l1 + (0.85 * D1)) * v1));
+            return F;
+
+        }
+        public static int AreaP(int sk, int ck)
+        {
+            int A = sk * ck;
+            return A;
+        }
+
+          
+    }
+}
---
+++ BoxMaster/MainActivity.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace BoxMaster
+{
+    
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView seView;
+        TextView seLength;
+        SeekBar sBar;
+        SeekBar vBar;
+        EditText vText; 
+         EditText   HZ;
+        EditText eSabwufer;
+        EditText eArea;
+                
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            HZ = FindViewById<EditText>(Resource.Id.HZ);
+            vText = FindViewById<EditText>(Resource.Id.vText);
+            sBar = FindViewById<SeekBar>(Resource.Id.sBar);
+            vBar = FindViewById<SeekBar>(Resource.Id.vBar);
+            seLength = FindViewById<TextView>(Resource.Id.seLength);
+            seView = FindViewById<TextView>(Resource.Id.seView);
+            eSabwufer = FindViewById<EditText>(Resource.Id.eSabwufer);
+            eArea = FindViewById<EditText>(Resource.Id.eArea);
+            HZ.TextChanged += HerzEdit;
+            vText.TextChanged += VoluemCharged;
+            sBar.ProgressChanged += sBarCharged;
+            vBar.ProgressChanged += vBarCharged;
+            eArea.TextChanged += eAreaCharged;
+            eSabwufer.TextChanged += eSabwuferCharged;
+
+            
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        void eAreaCharged(object sender, EventArgs e)
+        {
+            if ((eArea.Length() > 0) && (Convert.ToInt32(eArea.Text)!=0) && (eSabwufer.Length() > 0) && (Convert.ToInt32(eSabwufer.Text) != 0))
+            {
+                int x = Convert.ToInt32(Count.AreaP(Convert.ToInt32(eArea.Text), Convert.ToInt32(eSabwufer.Text)) * 0.6);
+                sBar.Max = x * 2;
+                sBar.Progress = x;
+            }
+        }
+
+
+        void eSabwuferCharged(object sender, EventArgs e)
+        {
+            if ((eArea.Length() > 0) && (Convert.ToInt32(eArea.Text) != 0) && (eSabwufer.Length() > 0) && (Convert.ToInt32(eSabwufer.Text) != 0))
+            {
+                int x = Convert.ToInt32(Count.AreaP(Convert.ToInt32(eArea.Text), Convert.ToInt32(eSabwufer.Text)) * 0.6);
+                sBar.Max = x * 2;
+                sBar.Progress = x;
+
+            }
+        }
+        void HerzEdit(object sender, EventArgs e)
+        {
+          
+
+        }
+        void VoluemCharged(object sender, EventArgs e)
+        {
+
+        }
+        void sBarCharged(object sender, EventArgs e)
+        {
+            if (vText.Length() > 0 && HZ.Length() > 0)
+            {
+                int s = Convert.ToInt32(vText.Text);
+                int F = Convert.ToInt32(HZ.Text);
+                if (s > 0 && F > 0)
+                {
+                    seView.Text = sBar.Progress.ToString() + " СМ^2";
+                    double l = Count.length(sBar.Progress, s, F);
+                    vBar.Max = (int)l * 2;
+                    vBar.Progress = (int)l;
+                }
+            }
+            //seLength.Text = l.ToString();
+        
+        }
+        void vBarCharged (object sender, EventArgs e)
+        {
+            int v = Convert.ToInt32(vText.Text);
+            int s = sBar.Progress;
+            seLength.Text = vBar.Progress.ToString()+"см";
+            int l = vBar.Progress;
+            HZ.Text = Convert.ToInt32(Count.Freq(l, s, v)).ToString();
+        }
+        
+
+    }
+
+    
+
+
+}
